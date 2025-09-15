c9b4a25a6768128f9de3253dd3d8ca7deb7eaa3d
Andugala Pujitha <149463713+AndugalaPujitha@users.noreply.github.com>
2025-09-15 16:19:28 +0530

    Timeline.tsx

--- pujitha-portfolio/src/components/Timeline.tsx
+++ pujitha-portfolio/src/components/Timeline.tsx
@@ -13,15 +13,40 @@ function Timeline() {
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2023 - 2025"
+            date="May 2025 - July 2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Internships</h3>
             <h4 className="vertical-timeline-element-subtitle">Bhimavarm, AP</h4>
             <p>
-              Skill-Dzire Machine Learning, Full-stack Development
+              Skill-Dzire Deep Learning.
             </p>
+          </VerticalTimelineElement>
+                    <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="May 2024 - July 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Internships</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bhimavarm, AP</h4>
+            <p>
+              Skill-Dzire Machine Learning.
+            </p>
+          </VerticalTimelineElement>
+                    <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="March 2024 - April 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Hackathons</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bhimavaram, AP</h4>
+            <p>• IITM-BUILDCLUB Machine Learning, IOT</p>
+            <p>Qualified for semi-finals in IITMIC ‘Build to Innovate’ (Rural-Agri Ed.) Challenge (April 2024).</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -34,8 +59,43 @@ function Timeline() {
             <h3 className="vertical-timeline-element-title">Hackathons</h3>
             <h4 className="vertical-timeline-element-subtitle">Bhimavaram, AP</h4>
             <p>• GDSC Full-stack Web Development,</p>
-            <p>• IITM-BUILDCLUB Machine Learning, IOT</p>
             <p>• Block Chain Project Management.</p>
+          </VerticalTimelineElement>
+                      <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2022 - 2026"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">B.Tech (CSE-AI & DS)</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bhimavaram, AP</h4>
+            <p>
+              Shri Vishnu Engineering College For Women.
+            </p>
+          </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2020 - 2022"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Intermediate (M.P.C)</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bhimavarm, AP</h4>
+            <p>
+              Tirumala Junior College.
+            </p>
+          </VerticalTimelineElement>
+            <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2019 - 2020"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Secondary School Certificate</h3>
+            <h4 className="vertical-timeline-element-subtitle">Kaikaluru, AP</h4>
+            <p>
+              Sri Chaitanya Techno School.
+            </p>
           </VerticalTimelineElement>
 
 
